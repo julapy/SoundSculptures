491667f7cb77f8722ef2b8790f0d43fd91a22d74
lukasz karluk <julapy@googlemail.com>
2014-11-01 16:21:13 +1100

    loading in different models.

--- 03_simpleMesh/src/ofApp.cpp
+++ 03_simpleMesh/src/ofApp.cpp
@@ -3,6 +3,35 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    ofLoadImage(meshTexture, "texture.jpg");
+    
+    //--------------------------------------------------------------
+    ofMesh customMesh;
+    customMesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+    
+    customMesh.addVertex(ofVec3f(-100, -100));
+    customMesh.addVertex(ofVec3f(-100, 100));
+    customMesh.addVertex(ofVec3f(100, -100));
+    customMesh.addVertex(ofVec3f(100, 100));
+    
+    customMesh.addTexCoord(ofVec2f(0, 0));
+    customMesh.addTexCoord(ofVec2f(0, 1));
+    customMesh.addTexCoord(ofVec2f(1, 0));
+    customMesh.addTexCoord(ofVec2f(1, 1));
+    
+    //--------------------------------------------------------------
+    ofSpherePrimitive sphere(100, 20);
+    ofCylinderPrimitive cylinder(100, 200, 20, 20);
+    
+    //--------------------------------------------------------------
+    model.loadModel("head.dae");
+    
+    //--------------------------------------------------------------
+    meshIndex = 0;
+    meshes.push_back(customMesh);
+    meshes.push_back(sphere.getMesh());
+    meshes.push_back(cylinder.getMesh());
+    meshes.push_back(model.getMesh(0));
 }
 
 //--------------------------------------------------------------
@@ -13,11 +42,46 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    ofEnableDepthTest();
+    
+    float dist = 400;
+    camera.setDistance(dist);
+    camera.begin();
+    
+    ofPushStyle();
+    
+    ofMesh & mesh = meshes[meshIndex];
+    
+    ofEnableNormalizedTexCoords();
+    meshTexture.bind();
+
+    ofSetColor(ofColor::white);
+    mesh.drawFaces();
+    
+    ofDisableNormalizedTexCoords();
+    meshTexture.unbind();
+    
+    ofSetColor(ofColor::black);
+    mesh.drawWireframe();
+    
+    ofPopStyle();
+    
+    camera.end();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if(key == OF_KEY_LEFT) {
+        meshIndex -= 1;
+        if(meshIndex < 0) {
+            meshIndex = meshes.size() - 1;
+        }
+    } else if(key == OF_KEY_RIGHT) {
+        meshIndex += 1;
+        if(meshIndex > meshes.size() - 1) {
+            meshIndex = 0;
+        }
+    }
 }
 
 //--------------------------------------------------------------
